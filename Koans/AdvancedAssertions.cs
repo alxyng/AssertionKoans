9c2e91111a219251197deb5cc5b31e762e59e80f
Robin Cox <robin.cox@myunidays.com>
2018-04-18 14:35:08 +0100

    Added 'advanced' assertion

--- Koans/AdvancedAssertions.cs
+++ Koans/AdvancedAssertions.cs
@@ -1,9 +1,34 @@
+using System;
 using AssertionKoans.Engine;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 
 namespace AssertionKoans.Koans
 {
 	class AdvancedAssertions : Koan
 	{
+		class TestObject
+		{
+			public int IntValue { get; set; }
+			public string StringValue { get; set; }
+
+			public float FloatValue => 3.14f;
+		}
+
+		[Step(1)]
+		public void ObjectEquivalence()
+		{
+			var thing = new TestObject { };
+  
+			thing.Should()
+			.BeOfType<TestObject>()
+			.And.BeEquivalentTo(new
+			{
+				IntValue = 5,
+				StringValue = "hello",
+				DateValue = 10.January(1986) // You'll need to add this property
+				// The float value doesn't matter
+			});
+		}
     }
 }
